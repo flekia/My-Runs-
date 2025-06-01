b6e1138c8112a0a4f9e82ced836255e72f360fee
flekia <alcaldeamir@yahoo.com>
2025-06-01 21:06:40 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,11 @@
-const CACHE_NAME = 'site-cache-v1';
-const FILES_TO_CACHE = [
-  './index.html', //inlined
-  './manifest.json' //manifest
-];
+var path = '/My-Runs-';
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
-});
+var appPrefix = 'myruns_';
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
+var version = 'version_00';
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return (
-        response || 
-        fetch(event.request).catch (() => {
-         if (event.request.mode === 'navigate') {
-         return caches.match('/index.html');
-         }
-        })
-    );
-    })
-  );
-});
+var urls = [
+  `${path}/`,
+  `${path}/index.html`,
+  `${path}/manifest.json`,
+]
