bac9bccaf1fb3d49b3210a28918040fa177b2799
flekia <alcaldeamir@yahoo.com>
2025-06-01 22:08:34 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,6 +17,19 @@ self.addEventListener('install', (event) => {
       })
     );
 });
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== 'my-cache') {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
