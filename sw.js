31caa8df89099481e7cef9066053c5b30644c923
flekia <alcaldeamir@yahoo.com>
2025-06-01 21:47:14 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,8 +10,12 @@ const urlsToCache = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+      .then(cache => {
+        return cache.addAll(urlsToCache))
+      }).catch(hehe => {
+        console.error('Failed caching', hehe);
+      })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
