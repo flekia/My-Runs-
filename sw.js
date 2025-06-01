ff6e8119175495199aefc176e40e90cf9f4c11c2
flekia <alcaldeamir@yahoo.com>
2025-06-01 21:20:18 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,3 +9,35 @@ var urls = [
   `${path}/index.html`,
   `${path}/manifest.json`,
 ]
+//caching
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(urls);
+    })
+  );
+});
+
+//uninstalls cache
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(
+        keyList.map(function(key) {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// saved
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
