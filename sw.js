197a813c5e4dbd81a2dcb7b288019068e020869e
flekia <alcaldeamir@yahoo.com>
2025-06-02 21:44:08 +0800

    fuck

--- sw.js
+++ sw.js
@@ -9,13 +9,19 @@ const urlsToCache = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => 
-        cache.addAll(urlsToCache)).catch(err => {
-          console.error('SW Failed', err)
-        })
-    );
+    caches.open(CACHE_NAME).then(async (cache) => {
+      for (const url of urlsToCache) {
+        try {
+          await cache.add(url);
+          console.log('Cached:', url);
+        } catch (err) {
+          console.error('Failed to cache:', url, err);
+        }
+      }
+    })
+  );
 });
+
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
